f2ebcbebff2515294ac6a7d20f3f3ec311b4c13c
23DP1EMozg <A230240EM@rvt.lv>
2025-06-10 23:41:37 +0300

    savienots login/register ar backend api

--- logins/app.js
+++ logins/app.js
@@ -10,18 +10,61 @@ sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
 
-       const loginForm = document.querySelector('.sign-in-form');
 
-      loginForm.addEventListener('submit', function (e) {
-        e.preventDefault(); // Neļauj formu nosūtīt
+      
 
-        const username = document.getElementById('login-username').value;
-        const password = document.getElementById('login-password').value;
+const register = async (email, password) => {
+    const response = await fetch("http://localhost:8080/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(
+        {
+        email: email,
+        password: password
+      }
+      )
+    })
 
-        // Šeit vari ievietot reālu login pārbaudi
-        if (username === "admin" && password === "1234") {
-          window.location.href = "index.html"; // Novirza uz mājaslapu
-        } else {
-          alert("Nepareizs lietotājvārds vai parole!");
-        }
-      });
+    if(response.status === 201){
+      alert("created!")
+    }
+}
+
+
+const login = async (email, password) => {
+    const response = await fetch("http://localhost:8080/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    })
+
+    if(response.status === 200){
+      const data = await response.json()
+      localStorage.setItem("user", data.jwt)
+      alert("logged in as: " + data.user.email)
+    }
+}
+
+
+document.getElementById("register-form").addEventListener('submit', async (e) => {
+  e.preventDefault()
+  const email = document.getElementById('email-register').value
+  const password = document.getElementById('password-register').value
+
+ await register(email, password)
+})
+
+
+document.getElementById('login-form').addEventListener('submit', async (e) => {
+  e.preventDefault()
+  const email = document.getElementById('email-login').value
+  const password = document.getElementById('password-login').value
+  await login(email, password)
+})
